b728d9f12503989bc14793ff8440842378f6085d
Edoardo Valentini <edoardo.valentini@live.it>
2022-03-21 11:09:11 +0100

    feature: add new endpoint for manually redirect to the right endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -6,38 +6,43 @@ const router = express.Router();
 //router allow us to create CRUD RESTful API
 module.exports = router;
 
-//Get Method
-router.get('/getInfo', async (req, res) => {
+//Manually redirect to the right endpoint
+router.get('/', (req, res) => {
+  res.send('Go to /api/getInfo to see something');
+}) /
 
-  const data = await Request.aggregate([
-    //filter between two date from req
-    { $match: { createdAt: { $gte: new Date(req.body.startDate), $lte: new Date(req.body.endDate) } } },
-    //sum all counts elements
-    { $addFields: { totalCount: { $sum: "$counts" } } },
-    //filter between two number from req
-    { $match: { totalCount: { $gte: req.body.minCount, $lte: req.body.maxCount } } },
-    //remove useless fields
-    { $unset: ["_id", "value", "counts"] }
-  ]);
+  //Get Method
+  router.get('/getInfo', async (req, res) => {
 
-  if (!data) {
-    throw new Error('Something goes wrong with aggregate');
-  }
+    const data = await Request.aggregate([
+      //filter between two date from req
+      { $match: { createdAt: { $gte: new Date(req.body.startDate), $lte: new Date(req.body.endDate) } } },
+      //sum all counts elements
+      { $addFields: { totalCount: { $sum: "$counts" } } },
+      //filter between two number from req
+      { $match: { totalCount: { $gte: req.body.minCount, $lte: req.body.maxCount } } },
+      //remove useless fields
+      { $unset: ["_id", "value", "counts"] }
+    ]);
 
-  try {
-    const response = {
-      code: res.statusCode,
-      msg: "Success",
-      records: data
-    };
-    res.send(response);
-  }
-  catch (error) {
-    const response = {
-      code: res.statusCode,
-      msg: error.message,
-      records: data
-    };
-    res.send(response);
-  }
-})
+    if (!data) {
+      throw new Error('Something goes wrong with aggregate');
+    }
+
+    try {
+      const response = {
+        code: res.statusCode,
+        msg: "Success",
+        records: data
+      };
+      res.send(response);
+    }
+    catch (error) {
+      const response = {
+        code: res.statusCode,
+        msg: error.message,
+        records: data
+      };
+      res.send(response);
+    }
+  })
